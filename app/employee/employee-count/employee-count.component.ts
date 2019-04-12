2b52c0963c096ab4fced4dd9d519e8be55b8fb4c
Tony Joseph <Tonyj@emids.com>
2019-04-12 19:22:04 +0530

    Angular learning 1

---
+++ app/employee/employee-count/employee-count.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-count',
+  templateUrl: './employee-count.component.html',
+  styleUrls: ['./employee-count.component.css']
+})
+export class EmployeeCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input()
+  all;
+
+  @Input()
+  male;
+
+  @Input()
+  female;
+}
